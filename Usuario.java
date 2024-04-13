1e1557bb34062290bd661d3b089c61c08b4706b8
PedroAOC20 <161269373+PedroAOC20@users.noreply.github.com>
2024-04-13 12:37:13 -0300

    Update Usuario.java
    Corrigido a edição de usuário do método "editarUser", onde foi adicionado as variáveis modificadoras.

--- Usuario.java
+++ Usuario.java
@@ -30,17 +30,27 @@ public class Usuario {
         }
     }
 
-    // Editar usuário
-    public static void editarUser(List<Usuario> listaUsuarios, Usuario user, Usuario admin) {
+    // Editar usuário - Metodo
+   public static void editarUser(List<Usuario> listaUsuarios, String id, String novoNome, String novoUsername, String novaSenha, Usuario admin) {
         if (admin.isAdmin) {
-            // Aqui você implementaria a lógica para editar um usuário
-            System.out.println("Usuário editado com sucesso!");
+            for (Usuario user : listaUsuarios) {
+                if (user.id.equals(id)) {
+                    // Modificar os dados do usuário
+                    user.nome = novoNome;
+                    user.username = novoUsername;
+                    user.senha = novaSenha;
+                    System.out.println("Usuário editado com sucesso!");
+                    return;
+                }
+            }
+            // Se o ID fornecido não corresponder a nenhum usuário
+            System.out.println("Usuário não encontrado.");
         } else {
             System.out.println("Você não tem permissão para editar usuários.");
         }
     }
 
-    // Excluir usuário
+    // Excluir usuário - Metodo
     public static void excluirUser(List<Usuario> listaUsuarios, Usuario user, Usuario admin) {
         if (admin.isAdmin) {
             listaUsuarios.remove(user);
@@ -50,7 +60,7 @@ public class Usuario {
         }
     }
 
-    // Atribuir permissão de administrador
+    // Atribuir permissão de administrador - Metodo
     public static void atribuirPermissao(Usuario user, boolean isAdmin, Usuario admin) {
         if (admin.isAdmin) {
             user.isAdmin = isAdmin;
@@ -60,7 +70,7 @@ public class Usuario {
         }
     }
 
-    // Consultar usuário
+    // Consultar usuário - Metodo
     public static void consultaUser(List<Usuario> listaUsuarios, Usuario user) {
         boolean encontrado = false;
         for (Usuario u : listaUsuarios) {
